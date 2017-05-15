1d826d170e634e063b7596dcfe3ed7d32a366711
dxjdqc <602951627@qq.com>
2017-05-15 10:23:52 +0800

    项目初次架构

---
+++ root-mobile-dxj/src/app.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <!-- 头部 -->
+        <mt-header title="传智21期CMS系统"></mt-header>
+        <router-view></router-view>
+        <!-- 底部 -->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-member"></span>
+                <span class="mui-tab-label">会员</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-shopcart"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">购物车</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-search"></span>
+                <span class="mui-tab-label">查找</span>
+            </a>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
